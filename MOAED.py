bf0c7cb9c5c34459e45b1dbffcfd30247605728d
YouennBogaer <163859975+YouennBogaer@users.noreply.github.com>
2026-01-11 17:28:24 +0100

    update method structure done, several functions added

--- MOAED.py
+++ MOAED.py
@@ -26,19 +26,20 @@ class MOEAD():
         self.N_ = len(self.weights)
         
         # Step 1.1 : Initialize EP (External population) as empty
+        # Will be the Pareto Front at the end
         self.ex_pop = []
         
         # Step 1.2 : Find T closest neighbors for each of the N weight vectors
+        # Stores the neighborrhood for each subproblems as an index of self.weights, and self.population
         self.B_ = self.neighborhood()
         
         # Step 1.3 : Generate an initial population
-        self.population = self.init_pop()
+        self.population, self.F_pop = self.init_pop()
         
         # Step 1.4 : Reference solution
         self.z_ = self.init_z()
     
     
-    # TODO
     # Step 2
     def update(self):
         for i in range(self.N_):
@@ -50,17 +51,55 @@ class MOEAD():
             # Step 2.2 : Improvement
             y_clean = self.repair(y)
             
-            # Step 2.3 : Update of
+            # Step 2.3 : Update of z
+            F_y = self.mop.evaluate(y_clean)
+            for m in range(self.dim):
+                if self.z_[m] < F_y[m]:
+                    self.z_[m] = F_y[m]
             
             # Step 2.4 : Update of neighboring solutions
-            
+            for j in self.B_[i]:
+                g_y_clean = self.tcheb_aggFunc(i,self.weights,F_y,self.z_)
+                g_x_j = self.tcheb_aggFunc(i,self.weights,self.F_pop[i],self.z_)
+
+                if g_y_clean <= g_x_j:
+                    self.population[j] = y_clean
+                    self.F_pop[j] = F_y
+                    
             # Step 2.5 Update of EP (External Population)
+            # Removes dominated solutions from External Population
+            self.ex_pop = [x for x in self.ex_pop if not self.is_dominated_by(x,y_clean)]
+            # Adds y_clean to the External Population if non dominated
+            if self.non_dominated_in_expop(y_clean):
+                self.ex_pop.append(y_clean)
+        
 
     # TODO
     # Step 3
     def execute(self):
         while not self.is_criterion_met():
             self.update()
+    
+    def tcheb_aggFunc(index, lambdas, F, z):
+        """
+        Compute the aggregation function following the Tchebycheff approach.
+        
+        Args:
+            index (int): current subproblem index (1 to N)
+            lambdas (List[Array]): weight vectors
+            F (Array): values of objective functions for current index
+            z (List[int]): reference
+
+        Returns:
+            int : value of the aggregation function, see Tchebycheff approach.
+        """
+        max_gap = 0
+        M = len(lambdas)
+        for m in range(M):
+            g = lambdas[index][m]*abs(F[m]-z[m])
+            if max_gap < g:
+                max_gap = g
+        return max_gap
     
     def select_mating(self, index):
         """
@@ -76,6 +115,37 @@ class MOEAD():
         j = rest_indices[j_id]
         
         return self.B_[index][i], self.B_[index][j]
+        
+    def is_dominated_by(self, x, y):
+        """
+        Determines if solution x is dominated by solution y.
+
+        Returns:
+            Bool
+        """
+        m = 0
+        F_x = self.mop.evaluate(x)
+        F_y = self.mop.evaluate(y)
+
+        while m<self.dim and F_x[m]<F_y[m]:
+            m += 1
+        if m==self.dim:
+            return True
+        else:
+            return False
+        
+    def non_dominated_in_expop(self, y):
+        """
+        Checks if solution y is non dominated
+        in the population self.ex_pop
+
+        Returns:
+            Bool
+        """
+        for x in self.ex_pop:
+            if self.is_dominated_by(y, x):
+                return False
+        return True
         
     # TODO
     def generate_solution(self, x1, x2):
@@ -102,7 +172,15 @@ class MOEAD():
     
     # TODO
     def init_pop(self):
-        return [np.array([0 for _ in range(self.dim)]) for _ in range(self.N_)]
+        """
+        Generates a population and stores each element by the MOP objective functions F.
+        Returns:
+            List[Array], List[Array]: population of solutions, evaluations by objective functions
+        """
+        # Need to figure out the generation of population
+        population = [np.array([0 for _ in range(self.dim)]) for _ in range(self.N_)]
+        F_pop = [self.mop.evaluate(x) for x in population]
+        return population, F_pop
     
     # TODO
     def init_z(self):
